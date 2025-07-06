e03ae32f5d1a79f7d26ce32028e3f0dbbad8ea53
fjdai <riptid3110@gmail.co>
2025-07-06 12:33:36 +0700

    fix màn hình trắng

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,21 +1,20 @@
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import { HelmetProvider } from 'react-helmet-async';
 import { createRouter } from './router';
-import { store } from './redux/store';
 import { CssBaseline } from '@mui/material';
 import { ToastContainer } from 'react-toastify';
+import { AppProviders } from './AppProviders.client';
 
 const router = createRouter();
 
 ReactDOM.hydrateRoot(
   document.getElementById('root')!,
-  <Provider store={store}>
+  <AppProviders>
     <HelmetProvider>
       <CssBaseline />
       <RouterProvider router={router} />
       <ToastContainer autoClose={2500} />
     </HelmetProvider>
-  </Provider>
+  </ AppProviders>
 ); 
--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,22 +1,21 @@
 import ReactDOMServer from 'react-dom/server';
 import { RouterProvider } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import { HelmetProvider } from 'react-helmet-async';
 import { createServerRouter } from './router';
-import { store } from './redux/store';
 import { CssBaseline } from '@mui/material';
+import { AppProviders } from './AppServerProvider';
 
 export function render(url: string) {
   const helmetContext = {};
   const router = createServerRouter(url);
   
   const html = ReactDOMServer.renderToString(
-    <Provider store={store}>
+    <AppProviders >
       <HelmetProvider context={helmetContext}>
         <CssBaseline />
         <RouterProvider router={router} />
       </HelmetProvider>
-    </Provider>
+      </AppProviders >
   );
 
   return { html, helmetContext };
