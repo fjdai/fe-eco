8012c02f7290592f43eb09e052eccf2133814ad3
fjdai <riptid3110@gmail.co>
2025-07-14 00:03:54 +0700

    icon

--- src/components/footer/EcommerceFooter.tsx
+++ src/components/footer/EcommerceFooter.tsx
@@ -9,7 +9,6 @@ import {
     List,
     ListItem,
     ListItemText,
-    Modal,
 } from '@mui/material';
 import {
     Facebook,
@@ -27,11 +26,8 @@ import {
     Verified
 } from '@mui/icons-material';
 import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
-import MailchimpForm from '../MailChimp/MailchimpForm';
 
 const EcommerceFooter = () => {
-    const [open, setOpen] = useState(false);
     const navigate = useNavigate();
     const footerSections = [
         {
@@ -268,33 +264,6 @@ const EcommerceFooter = () => {
 
                     {/* Newsletter Signup */}
                     <Grid item xs={12} md={4}>
-                        <Button 
-                        variant="outlined" 
-                        onClick={() => setOpen(true)}
-                        sx={{ mt: 2 }}
-                    >
-                        Đăng ký nhận tin
-                    </Button>
-
-                    {/* Popup Modal */}
-                    <Modal open={open} onClose={() => setOpen(false)}>
-                        <Box
-                        sx={{
-                            position: 'absolute',
-                            top: '50%',
-                            left: '50%',
-                            transform: 'translate(-50%, -50%)',
-                            width: 400,
-                            bgcolor: 'background.paper',
-                            borderRadius: 2,
-                            boxShadow: 24,
-                            p: 4,
-                        }}
-                        >
-                        <MailchimpForm />
-                        </Box>
-                    </Modal>
-
                         {/* Download App */}
                         <Typography variant="h6" sx={{ fontWeight: 600, mb: 2, color: '#3498db' }}>
                             Tải ứng dụng
